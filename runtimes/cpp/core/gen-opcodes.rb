4186aa5f613d48a2600b8f598cc67c00a62cc58c
Fredrik Eldh <fredrik.eldh@mobilesorcery.com>
2012-06-20 17:32:41 +0200

    gen-opcodes: new mode binutils/desc.

--- runtimes/cpp/core/gen-opcodes.rb
+++ runtimes/cpp/core/gen-opcodes.rb
@@ -4,29 +4,36 @@
 # Outputs header files with enums in the following languages:
 # CGEN
 # C
+# custom binutils description
+
+Opcode = Struct.new('Opcode', :name, :mnemonic, :operands)
+
+def o(n,m,o)
+	Opcode.new(n,m,o)
+end
 
 OPCODES = [
-	:nop,	# NOP must be the first one, number 0.
-	:ldr,
-	:ldi,
-	:ldw,
-	:ldb,
-	:ldh,
-	:stw,
-	:stb,
-	:sth,
-	:jpr,
-	:jpi,
-	:callr,
-	:calli,
-	:syscall,
-	:ret,
-	:push,
-	:pop,
-	:not,
-	:neg,
-	:xh,
-	:xb,
+	o(:nop, 'nop', []),	# NOP must be the first one, number 0.
+	o(:ldr, 'ld', [:rd, :rs]),
+	o(:ldi, 'ld', [:rd, :imm]),
+	o(:ldw, 'ld', [:rd, :adaddr]),
+	o(:ldb, 'ld.h', [:rd, :adaddr]),
+	o(:ldh, 'ld.b', [:rd, :adaddr]),
+	o(:stw, 'ld', [:adaddr, :rs]),
+	o(:stb, 'ld.b', [:adaddr, :rs]),
+	o(:sth, 'ld.h', [:adaddr, :rs]),
+	o(:jpr, 'jp', [:rd]),
+	o(:jpi, 'jp', [:aiaddr]),
+	o(:callr, 'call', [:rd]),
+	o(:calli, 'call', [:aiaddr]),
+	o(:syscall, 'syscall', [:imm8]),
+	o(:ret, 'ret', []),
+	o(:push, 'push', [:rd, :rs]),
+	o(:pop, 'pop', [:rd, :rs]),
+	o(:not, 'not', [:rd, :rs]),
+	o(:neg, 'neg', [:rd, :rs]),
+	o(:xh, 'xh', [:rd, :rs]),
+	o(:xb, 'xb', [:rd, :rs]),
 ]
 
 JC_OPCODES = [
@@ -42,8 +49,8 @@ JC_OPCODES = [
 	:leu,
 ]
 
-JC_OPCODES.each do |o|
-	OPCODES << ('jc_' + o.to_s).to_sym
+JC_OPCODES.each do |op|
+	OPCODES << o(('jc_' + op.to_s).to_sym, 'jc '+op.to_s, [:rd, :rs, :riaddr])
 end
 
 ARITH_OPCODES = [
@@ -60,9 +67,10 @@ ARITH_OPCODES = [
 	:srl,
 ]
 
-ARITH_OPCODES.each do |o|
-	OPCODES << o
-	OPCODES << (o.to_s + 'i').to_sym
+ARITH_OPCODES.each do |op|
+	OPCODES << o(op, op.to_s, [:rd, :rs])
+	i = op.to_s + 'i'
+	OPCODES << o(i.to_sym, op.to_s, [:rd, :imm])
 end
 
 puts "Count: #{OPCODES.size} opcodes"
@@ -73,28 +81,107 @@ filename = ARGV[1]
 puts "Mode: #{mode}"
 puts "Filename: #{filename}"
 
+def nodePart(name, children_or_opcode)
+	name = name.gsub('.', '_')
+	name = name.gsub(' ', '_')
+	if(children_or_opcode.is_a? Hash)
+		return "#{name}, ARRAY_SIZE(#{name})"
+	else
+		return "NULL, OP_#{children_or_opcode.to_s.upcase}"
+	end
+end
+
+def outputParseNode(file, prefix, op)
+	prefix = prefix.gsub('.', '_')
+	prefix = prefix.gsub(' ', '_')
+	if(op.is_a? Hash)
+		op.each do |operand, children|
+			outputParseNode(file, prefix + '_' + operand.to_s, children)
+		end
+		file.puts "static const mapip2_parse_node _#{prefix}[] = {"
+		op.each do |operand, children_or_opcode|
+			c = nodePart("_#{prefix}_#{operand}", children_or_opcode)
+			file.puts "\t{ #{operand.to_s.upcase}, #{c} },"
+		end
+		file.puts '};'
+	end
+end
+
 if(mode == 'cgen')
-	open(filename, 'w') do |cgen|
+	open(filename, 'w') do |file|
 		i = 0
-		cgen.puts '(define-normal-insn-enum opcodes "opcodes" () OP_ f-op ('
-		OPCODES.each do |o|
-			cgen.puts "(\"#{o.to_s.upcase}\" #{i})"
+		file.puts '(define-normal-insn-enum opcodes "opcodes" () OP_ f-op ('
+		OPCODES.each do |op|
+			file.puts "(\"#{op.name.to_s.upcase}\" #{i})"
 			i += 1
 		end
-		cgen.puts '))'
+		file.puts '))'
 	end
 elsif(mode == 'ccore')
-	open(filename, 'w') do |cgen|
+	open(filename, 'w') do |file|
 		i = 0
-		cgen.puts '#ifndef GEN_OPCODES_H'
-		cgen.puts '#define GEN_OPCODES_H'
-		cgen.puts
-		OPCODES.each do |o|
-			cgen.puts "static const int OP_#{o.to_s.upcase} = #{i};"
+		file.puts '#ifndef GEN_OPCODES_H'
+		file.puts '#define GEN_OPCODES_H'
+		file.puts
+		OPCODES.each do |op|
+			file.puts "#define OP_#{op.name.to_s.upcase} #{i}"
 			i += 1
 		end
-		cgen.puts
-		cgen.puts '#endif	//GEN_OPCODES_H'
+		file.puts
+		file.puts '#endif	//GEN_OPCODES_H'
+	end
+elsif(mode == 'binutils/desc')
+	open(filename, 'w') do |file|
+		file.puts '#ifndef _MAPIP2_DESC_H'
+		file.puts "#error Don't include this file unless you know what you're doing."
+		file.puts '#endif'
+		file.puts
+		# plain opcode table
+		file.puts 'static const mapip2_insn mapip2_insns[] = {'
+		OPCODES.each do |op|
+			file.puts "{ OP_#{op.name.to_s.upcase}, \"#{op.mnemonic}\", { "+
+				op.operands.collect { |r| r.to_s.upcase + ', ' }.join + "END } },"
+		end
+		file.puts '};'
+		file.puts
+		# parse tree
+		# eg: 'ld' => [
+		#	rd => [rs, imm, adaddr],
+		#	adaddr => [rs],
+		# ]
+		tree = {}
+		OPCODES.each do |op|
+			node = tree[op.mnemonic]
+			node = {} if(!node)
+			n0 = node
+			n2 = node
+			nr = nil
+			op.operands.each do |r|
+				n1 = n0[r]
+				n1 = {} if(!n1)
+				n0[r] = n1
+				n2 = n0
+				nr = r
+				n0 = n1
+			end
+			if(nr)
+				n2[nr] = op.name
+			else
+				node = op.name
+			end
+			tree[op.mnemonic] = node
+		end
+		p tree
+		puts "Mnemonics: #{tree.size}"
+		tree.each do |m, op|
+			outputParseNode(file, m.to_s, op)
+		end
+		# todo: use gperf to make a perfect hash table instead of an array.
+		file.puts 'static const mapip2_mnemonic mapip2_mnemonics[] = {'
+		tree.each do |m, op|
+			file.puts "{ \"#{m}\", #{nodePart("_#{m}", op)} },"
+		end
+		file.puts '};'
 	end
 else
 	raise "Invalid mode: #{mode}"
